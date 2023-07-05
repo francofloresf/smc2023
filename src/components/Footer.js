f0053826662d1041e3b16c026795685ffc414acc
Franco Flores <francofloresf@gmail.com>
2023-07-05 07:52:06 -0300

    fix title page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,6 @@ const Footer = () =>{
                     <i>Ilumina tu vida</i>
                 </div>
             </div>
-            <div className="instagram">
-                <a href="/https://www.instagram.com/fraguflores/">
-                    <i className="fa-brands fa-instagram-square"></i>
-                </a>
-            </div>
         </div>
         </div>
     )
